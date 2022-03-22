e4d3a5b8803fd81fb1a721cc5df2d390c95d4d0b
qwakumuller <richmondawuahadjei@yahoo.com>
2022-03-22 14:40:44 -0400

    Add Endpoints

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,8 +1,8 @@
 export const environment = {
   production: true,
   GATEWAY_URI: "http://35.223.254.189:8084/",
-  USER_URI: "http://34.71.225.31:8081/",
-  POST_URI: "http://34.132.249.24:8000/",
-  LIST_URI: "http://34.71.225.31:8081/",
-  COMMENT_URI: "http://34.132.249.24:8000/"
+  USER_URI: "http://34.121.99.46:8081/",
+  POST_URI: "http://35.192.86.61:8000/",
+  LIST_URI: "http://34.121.99.46:8081/",
+  COMMENT_URI: "http://35.192.86.61:8000/"
 };
